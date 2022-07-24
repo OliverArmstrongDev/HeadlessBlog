493e65567ed2b40d2da77bdaf14f790a3456873b
Oliver Armstrong <osarmstrong@gmail.com>
2022-07-24 19:56:36 +1200

    404

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,36 @@
+import {useEffect} from 'react';
+import Link from "next/link";
+import {useRouter} from "next/router";
+
 const NotFound = () => {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/')
+    },3000)
+  },[])
+
     return ( 
-     
       <div className="not-found">
-        <h2>Ooooops that was not found!</h2>
+        <h1>404</h1>
+        <h2>Ooooops that was not found! :(</h2>
+        <p>Redirecting to <Link href={'/'}>Homepage</Link></p>
+
+        <style jsx>{`
+        .not-found{
+          background: #fff;
+          padding: 30px;
+          box-shadow: 5px 10px rgba(0,0,0,.5);
+          text-align: center;
+          transform: rotateZ(-1deg);
+          border-radius: 12px;
+        }
+        h1{
+          font-size: 3em;
+        }
+        `}</style>
       </div>
          );
 }
